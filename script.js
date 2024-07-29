067aa95bb233a50e3d059099eac704a406579b2c
OlYeromin <ol.yeromin@gmail.com>
2024-07-29 12:02:38 +0300

    Add a function to get a random integer

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// The function below is taken from here:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}  
+ 
 function getComputerChoice() {
     computerChoice = getRandom(3);
     switch (computerChoice) {
