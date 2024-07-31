74d36ec7ce4290920e55358a85190ee02af021e4
OlYeromin <ol.yeromin@gmail.com>
2024-07-31 11:18:34 +0300

    Add function that appends p tags with text

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function appendPara(text, parent) {
+    const para = document.createElement("p");
+    para.textContent = text;
+    parent.appendChild(para);    
+}
+
 // The function below is taken from here:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(max) {
